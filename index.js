b50f6b80e36401b4fecd27dc8ddea5745c425942
okancancoskun <okancan1998@hotmail.com>
2021-03-04 12:55:16 +0300

    http-module

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    const options = {
+        hostname: 'localhost',
+        port: 5000,
+        path: req.url
+    }
+
+    const proxy = http.get(options, (request, response) => {
+        request.pipe(res, {
+            end: true
+        })
+    })
+    req.pipe(proxy, { end: true })
+}).listen(3000, () => console.log('server started'))
